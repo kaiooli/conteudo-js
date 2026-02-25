6754a22c908638ee506c95c0dfdd1ce98ff84aa4
kaio Oli <kaionascimentooliveira8@gmail.com>
2026-02-24 22:19:49 -0300

    aula 1 jv

---
+++ src/script.js
@@ -0,0 +1,15 @@
+//Exemplos de Variável let
+console.log('Testando JavaScript');
+
+let nome = 'Kaio';
+
+let nome2 = 'Richard';
+
+console.log(nome2);
+
+//Exemplos de variável const
+
+const times = ['São Paulo', 'Corinthans', 'Santos'];
+
+
+console.log(times);
