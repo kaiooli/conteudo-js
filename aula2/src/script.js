ab00254e8eba83c5194bab7ae54bfb7b6688d85d
kaio Oli <kaionascimentooliveira8@gmail.com>
2026-02-25 21:16:50 -0300

    JavaScript 25/02

---
+++ aula2/src/script.js
@@ -0,0 +1,108 @@
+//Exemplos de operador de atribuição
+let x = 100;
+
+x = x - 5;
+//console.log(x);
+
+x = x + 5;
+//console.log(x);
+
+//Exemplos de Aritimética em JavaScript
+let soma = 5 + 5 + 4;
+console.log(soma);
+
+const primeiroNome = 'Kaio';
+const ultimoNome = 'Oliveira';
+
+//let nomeCompleto = primeiroNome + ultimoNome;
+
+let nomeCompleto = 'Kaio' + " " +'Oliveira';
+
+console.log(primeiroNome);
+console.log(ultimoNome);
+
+
+//Exemplos de tipos em JavaScript
+//String
+let cor = 'Azul';
+let fruta = 'Laranja';
+console.log(cor);
+console.log(fruta);
+
+//Number
+let soma1 = 5 + 5;
+let subtracao = 10 - 3;
+console.log(soma1);
+console.log(subtracao);
+
+//bigInt
+let biginit = 9459804519487089197540349898543534534;
+let bigInt2 = BigInt(9459804519487089197540349898543534534);
+console.log(biginit);
+console.log(bigInt2);
+
+//Boolean
+let valorverdadeiro = true;
+let vlorfalso = false;
+console.log(valorverdadeiro);
+console.log(vlorfalso);
+
+//Object
+const carro = {
+    marca: 'Porshe',
+    modelo: '911 Turbo S',
+    cor: 'Vermelho',
+}
+
+console.log(carro);
+
+console.log('A marca do carro é:' + carro.marca);
+
+
+//Array
+const frutas = ['Lranja', 'Banana', 'Uva', 'Morango', 'Abacaxi'];
+
+console.log(frutas);
+
+//Date Object
+const data = new Date;
+
+console.log(data);
+
+// Undefined
+let indefinido;
+
+console.log(indefinido);
+
+// Null 
+let nulo = null;
+
+console.log(nulo);
+
+// Symbol
+let y = Symbol();
+
+console.log(y);
+
+//Exemplos de typeof
+let stringTipo = "300";
+let numberTipo = 300;
+
+//Exemplos de tipoDado e typeof
+const tipoDado = typeof numberTipo
+console.log(tipoDado);
+
+//Exemplos de Boolean
+let cinco = 5;
+let comparacao = cinco == 8;
+
+console.log(comparacao);
+
+
+
+
+
+
+
+
+
