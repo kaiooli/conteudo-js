3d2b815998c2454993766b9087dc2783fc5dc227
kaio Oli <kaionascimentooliveira8@gmail.com>
2026-03-09 19:56:05 -0300

    Aula 9 - Métodos de Number

---
+++ aula9/src/script2.js
@@ -0,0 +1,37 @@
+//Utilização do método toString()
+
+let numero = 123;
+let exToString = numero.toString();
+console.log(typeof exToString);
+
+//Utilização do método toExponential()
+
+let a = 9.646;
+let extoExponential = a.toExponential(2)
+console.log(extoExponential);
+
+//Utilização do toFixed()
+
+let b = 6.2022;
+let exToFixed = b.toFixed(1);
+
+console.log(exToFixed);
+
+//Utilização do método toPrecision()
+let c = 45.65486626;
+let exToPrecision = c.toPrecision(4);
+console.log(exToPrecision);
+
+//Utilização do método valueOf()
+
+let d = 100.000;
+let exValueOf = d.valueOf();
+console.log(exValueOf);
+
+//Utilização do método Number()
+
+let e = "2007";
+let exNumber = Number(e);
+console.log(exNumber);
+
+//Utilização do método ()
