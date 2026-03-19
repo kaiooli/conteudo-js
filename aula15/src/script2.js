d4f05808f28c926bb2519123846186711b51132e
kaio Oli <kaionascimentooliveira8@gmail.com>
2026-03-18 21:22:08 -0300

    Aula 15 estudando o DOM

---
+++ aula15/src/script2.js
@@ -0,0 +1,19 @@
+//Utilizando o getElementsById()
+const elemento1 = document.getElementById("selecionandoElemento");
+
+console.log(elemento1);
+
+//Utlizando o getElementsByIdTgName
+const elemento2 = document.getElementsByTagName("h1");
+
+console.log(elemento2[1]);
+
+//Utlizando o getElementsByClassName()
+const elemento3 = document.getElementsByClassName("sEP");
+
+console.log(elemento3[1]);
+
+//Utilizando o querySelector()
+const elemento4 = document.querySelector("#elementoClasse");
+
+
